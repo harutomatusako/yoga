73f51308860af1da1e04a6193876eaa25d4d1c94
EC2 Default User <ec2-user@ip-172-31-36-149.ap-northeast-1.compute.internal>
2022-01-29 09:27:49 +0000

    root変更

--- db/migrate/20220106113834_create_event_joins.rb
+++ db/migrate/20220106113834_create_event_joins.rb
@@ -1,7 +1,7 @@
 class CreateEventJoins < ActiveRecord::Migration[5.2]
   def change
     create_table :event_joins do |t|
-      
+
       t.references :user, foreign_key: true
       t.references :event, foreign_key: true
       t.text :status
