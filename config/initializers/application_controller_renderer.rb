0283da5e1d860664e795b7668792af6ee320f47d
EC2 Default User <ec2-user@ip-172-31-36-149.ap-northeast-1.compute.internal>
2022-01-29 11:06:41 +0000

    add refile secretkey

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,4 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+Refile.secret_key = 'aa788897924678bd201d7ccd7f954cce1f16bc5f9854b89fd69ca6af6ec5685cfebd7153bdd00170599195aa68f1952501b5f714672f55ecf96cfffa59f9ac54'
